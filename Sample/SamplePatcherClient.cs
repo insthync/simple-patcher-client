5fa481f2fe0ee37dae057aa144ca229428364839
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-06-23 21:44:45 +0700

    Update demo content

--- Sample/SamplePatcherClient.cs
+++ Sample/SamplePatcherClient.cs
@@ -1,18 +1,20 @@
-﻿using System.Collections;
+﻿using SimplePatcher;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SamplePatcherClient : MonoBehaviour
 {
+    public SimplePatcherClient client;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        client.StartUpdate();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnClickQuit()
     {
-        
+        Application.Quit();
     }
 }

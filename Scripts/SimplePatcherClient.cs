6d425cfe5acaa0a227708cb10e2b0183e387f9af
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-06-23 17:17:53 +0700

    WIP partial downloading

--- Scripts/SimplePatcherClient.cs
+++ Scripts/SimplePatcherClient.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Net;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Networking;
@@ -10,11 +13,13 @@ namespace SimplePatcher
     public class SimplePatcherClient : MonoBehaviour
     {
         public class StringEvent : UnityEvent<string> { }
-        [System.Serializable]
+        public class ProgressEvent : UnityEvent<long, long> { }
+        [Serializable]
         public struct ValidateResult
         {
             public bool updated;
             public string fileurl;
+            public string filemd5;
         }
         
         public enum State
@@ -25,29 +30,35 @@ namespace SimplePatcher
             Unzipping,
             ReadyToPlay
         }
-        public string saveLatestMD5File = "md5.txt";
+        public string cachingMD5File = "downloading_md5.txt";
+        public string unzippedMD5File = "unzipped_md5.txt";
+        public string cachingZipFile = "local.zip";
         public string serviceUrl = "ENTER YOUR SERVICE URL HERE";
         public StringEvent onCompareMD5Error;
+        public ProgressEvent onDownloadingProgress;
+        public UnityEvent onDownloadingFileNotExisted;
 
         public State CurrentState { get; private set; }
 
         public void StartUpdate()
         {
-            string md5 = string.Empty;
-            string combinedPath = Path.Combine(Application.dataPath, Path.GetFileName(saveLatestMD5File));
-            if (File.Exists(combinedPath))
-            {
-                StreamReader reader = new StreamReader(combinedPath);
-                md5 = reader.ReadToEnd();
-                reader.Close();
-            }
-            StartCoroutine(StartUpdateRoutine(md5));
+            StartCoroutine(StartUpdateRoutine());
         }
 
-        IEnumerator StartUpdateRoutine(string lastUpdateMD5)
+        IEnumerator StartUpdateRoutine()
         {
             CurrentState = State.ValidatingMD5;
-            UnityWebRequest request = new UnityWebRequest(serviceUrl + "?md5=" + lastUpdateMD5);
+            string md5 = string.Empty;
+            string unzippedMD5File = GetUnzippedMD5FilePath();
+            if (File.Exists(unzippedMD5File))
+            {
+                using (StreamReader reader = new StreamReader(unzippedMD5File))
+                {
+                    md5 = reader.ReadToEnd();
+                    reader.Close();
+                }
+            }
+            UnityWebRequest request = new UnityWebRequest(serviceUrl + "?md5=" + md5);
             yield return request.SendWebRequest();
             if (request.isHttpError || request.isNetworkError)
             {
@@ -59,7 +70,7 @@ namespace SimplePatcher
                 if (!result.updated)
                 {
                     CurrentState = State.Downloading;
-                    yield return StartCoroutine(DownloadFileRoutine(result.fileurl));
+                    yield return StartCoroutine(DownloadFileRoutine(result.fileurl, result.filemd5));
                     CurrentState = State.Unzipping;
                     yield return StartCoroutine(UnzipRoutine());
                 }
@@ -67,16 +78,98 @@ namespace SimplePatcher
             CurrentState = State.ReadyToPlay;
         }
 
-        IEnumerator DownloadFileRoutine(string url)
+        IEnumerator DownloadFileRoutine(string url, string serviceMD5)
         {
-            // TODO: Implement this
-            yield return null;
+            string cachingMD5 = string.Empty;
+            string cachingMD5File = GetCachingMD5FilePath();
+            if (File.Exists(cachingMD5File))
+            {
+                using (StreamReader reader = new StreamReader(cachingMD5File))
+                {
+                    cachingMD5 = reader.ReadToEnd();
+                    reader.Close();
+                }
+            }
+            string cachingFilePath = GetCachingFilePath();
+            bool cachingFileExists = File.Exists(cachingFilePath);
+            if (cachingFileExists && !serviceMD5.Equals(cachingMD5))
+            {
+                // Clear old partial downloading file
+                File.Delete(cachingFilePath);
+                cachingFileExists = false;
+            }
+            long cachingFileSize;
+            long downloadingFileSize;
+            do
+            {
+                cachingFileSize = cachingFileExists ? new FileInfo(cachingFilePath).Length : 0;
+                // Get downloading file size
+                HttpWebRequest headRequest = (HttpWebRequest)WebRequest.Create(url);
+                headRequest.Method = WebRequestMethods.Http.Head;
+                HttpWebResponse headResponse;
+                try
+                {
+                    headResponse = (HttpWebResponse)headRequest.GetResponse();
+                    headRequest.Abort();
+                }
+                catch
+                {
+                    // Cannot find the file from service
+                    onDownloadingFileNotExisted.Invoke();
+                    yield break;
+                }
+                downloadingFileSize = headResponse.ContentLength;
+                yield return null;
+                onDownloadingProgress.Invoke(cachingFileSize, downloadingFileSize);
+                if (cachingFileSize != downloadingFileSize)
+                    DownloadFile(cachingFilePath, url, cachingFileSize, downloadingFileSize);
+            } while (cachingFileSize != downloadingFileSize);
+        }
+
+        void DownloadFile(string cachingFilePath, string downloadingFileUrl, long cachingFileSize, long downloadingFileSize)
+        {
+            int bufferSize = 1024 * 1000;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(downloadingFileUrl);
+            request.Timeout = 30000;
+            request.AddRange((int)cachingFileSize, (int)downloadingFileSize - 1);
+            request.Method = WebRequestMethods.Http.Get;
+            WebResponse response = request.GetResponse();
+            Stream inStream = response.GetResponseStream();
+            FileStream outStream = new FileStream(cachingFilePath, (cachingFileSize > 0) ? FileMode.Append : FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+
+            int count;
+            byte[] buff = new byte[bufferSize];
+            while ((count = inStream.Read(buff, 0, bufferSize)) > 0)
+            {
+                outStream.Write(buff, 0, count);
+                outStream.Flush();
+            }
+
+            outStream.Flush();
+            outStream.Close();
+            inStream.Close();
+            request.Abort();
         }
 
         IEnumerator UnzipRoutine()
         {
             // TODO: Implement this
             yield return null;
+        }
+
+        string GetUnzippedMD5FilePath()
+        {
+            return Path.Combine(Application.dataPath, Path.GetFileName(unzippedMD5File));
+        }
+
+        string GetCachingMD5FilePath()
+        {
+            return Path.Combine(Application.dataPath, Path.GetFileName(cachingMD5File));
+        }
+
+        string GetCachingFilePath()
+        {
+            return Path.Combine(Application.dataPath, Path.GetFileName(cachingZipFile));
         }
     }
 }
